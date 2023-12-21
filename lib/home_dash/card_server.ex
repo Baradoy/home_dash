114e32b0e53c051f5e52aa6587c10555876c67d1
Graham Baradoy <graham@baradoy.ca>
2023-12-21 13:24:00 -0800

    Add CardServer to abstract subscription behaviour

---
+++ lib/home_dash/card_server.ex
@@ -0,0 +1,64 @@
+defmodule HomeDash.CardServer do
+  @moduledoc """
+    A server to keep card state and send state to subscribers.
+
+    Implementation note:
+      Card providers like WelcomeCardProvider interact with this service through the API and server PID.
+      Another possible implementation would be to go the metaprogramming route.
+  """
+
+  use GenServer
+
+  def start_link(namespace) when is_atom(namespace) do
+    GenServer.start_link(__MODULE__, namespace)
+  end
+
+  def subscribe(client_pid, server_pid) do
+    GenServer.cast(server_pid, {:subscribe, client_pid})
+  end
+
+  @impl true
+  def init(namespace) do
+    Process.flag(:trap_exit, true)
+    state = %{namespace: namespace, cards: [], subscriptions: []}
+    {:ok, state}
+  end
+
+  def push_card(card, pid) do
+    GenServer.cast(pid, {:push_card, card})
+  end
+
+  @impl true
+  def handle_cast({:push_card, card}, state) do
+    state = Map.put(state, :cards, [card |state.cards])
+    {:noreply, state, {:continue, {:push_card, card}}} |> dbg
+  end
+
+  @impl true
+  def handle_cast({:subscribe, pid}, state) do
+    state = Map.put(state, :subscriptions, [pid |state.subscriptions])
+    {:noreply, state, {:continue, {:subscribe, pid}}} |> dbg
+  end
+
+  @impl true
+  def handle_info({:EXIT, pid, _reason}, state) do
+    state = Map.put(state, :subscriptions, List.delete(state.subscriptions, pid))
+    {:noreply, state} |> dbg
+  end
+
+  @impl true
+  def handle_continue({:subscribe, client_pid}, state) do
+    send(client_pid, {:home_dash, :initial, state.cards})
+    Process.link(client_pid)
+
+    {:noreply, state}
+  end
+
+  def handle_continue({:push_card, card}, state) do
+    Enum.each(state.subscriptions, fn pid ->
+      send(pid, {:home_dash, :new, card})
+    end)
+
+    {:noreply, state}
+  end
+end
--- lib/home_dash/welcome_card_provider.ex
+++ lib/home_dash/welcome_card_provider.ex
@@ -1,53 +1,38 @@
 defmodule HomeDash.WelcomeCardProvider do
   use GenServer
 
-  def subscribe() do
-    pid = Process.whereis(HomeDash.WelcomeCardProvider)
-    Process.link(pid)
+  @namespace :welcome
+
+  # API
+
+  def subscribe(pid \\ __MODULE__) do
     GenServer.cast(pid, {:subscribe, self()})
   end
 
-  def start_link(state) when is_list(state) do
-    GenServer.start_link(__MODULE__, state, name: __MODULE__)
-  end
-
-  @impl true
-  def init(state) do
-    Process.flag(:trap_exit, true)
-    {:ok, {[], state}}
-  end
-
-  def push_card(card , pid \\ __MODULE__) do
+  def push_card(card, pid \\ __MODULE__) do
     GenServer.cast(pid, {:push_card, card})
   end
 
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  # Server
+
   @impl true
-  def handle_cast({:push_card, card}, {subscriptions, state}) do
-    {:noreply, {subscriptions, [card | state]}, {:continue, {:push_card, card}}}
+  def init(_state) do
+    {:ok, pid} = HomeDash.CardServer.start_link(@namespace)
+    {:ok, %{server_pid: pid}}
   end
 
   @impl true
-  def handle_cast({:subscribe, pid}, {subscriptions, state}) do
-    {:noreply, {[pid | subscriptions], state}, {:continue, {:subscribe, pid}}}
+  def handle_cast({:subscribe, pid}, state) do
+    HomeDash.CardServer.subscribe(pid, state.server_pid)
+    {:noreply, state}
   end
 
-  @impl true
-  def handle_info({:EXIT, pid, _reason}, {subscriptions, state}) do
-    {:noreply, {List.delete(subscriptions, pid), state}} |> dbg
-  end
-
-  @impl true
-  def handle_continue({:subscribe, pid}, {subscriptions, state}) do
-    send(pid, {:home_dash, :initial, state})
-
-    {:noreply, {subscriptions, state}}
-  end
-
-  def handle_continue({:push_card, card}, {subscriptions, state}) do
-    Enum.each(subscriptions, fn pid ->
-      send(pid, {:home_dash, :new, card})
-    end)
-
-    {:noreply, {subscriptions, state}}
+  def handle_cast({:push_card, card}, state) do
+    HomeDash.CardServer.push_card(card, state.server_pid)
+    {:noreply, state}
   end
 end
