db034e7255f79afa4ed75502ee599162cbf739b5
Hez Ronningen <code@hez.ca>
2024-05-30 09:59:26 -0700

    refactoring how the abv makes its way in to the message attr

--- lib/home_dash_web/components/cards/brew_dash_taps.ex
+++ lib/home_dash_web/components/cards/brew_dash_taps.ex
@@ -11,22 +11,22 @@ defmodule HomeDashWeb.Cards.BrewDashTaps do
     assigns =
       assigns
       |> assign(:image_url, Map.get(brew, "image_url"))
+      |> assign(:abv, Map.get(brew, "abv"))
       |> assign_new(:tap_number, fn -> Map.get(brew, "tap_number") end)
       |> assign_new(:status_badge, fn -> Map.get(brew, "status_badge") end)
       |> assign_new(:status_badge_present, fn
         %{tap_number: tn, status_badge: "ON TAP"} when not is_nil(tn) -> true
         _ -> false
       end)
+      |> assign_new(:message, fn
+        %{abv: :unknown} -> "ABV: -"
+        %{abv: abv} -> "ABV: #{abv}%"
+      end)
       |> assign(:is_gf, Map.get(brew, "is_gf"))
       |> assign(:full_name, Map.get(brew, "full_name"))
-      |> assign(:abv, Map.get(brew, "abv"))
 
     ~H"""
-    <Default.render
-      card={@card}
-      title={@full_name}
-      message={if @abv != :unknown, do: "ABV: #{@abv}%", else: "ABV: -"}
-    >
+    <Default.render card={@card} title={@full_name} message={@message}>
       <:image>
         <img class="h-96 w-full object-cover" src={@image_url} alt="Recipe Picture" />
       </:image>
